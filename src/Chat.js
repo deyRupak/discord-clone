1b8484968783c753fc580b441dba1600ced8d70a
deyRupak <rupakdey9999@gmail.com>
2020-10-13 19:58:11 +0530

    Add Msg Input

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import './Chat.css'
 import ChatHeader from './ChatHeader'
-
+import Message from './Message'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader />
+
+            <div className="chat__messages">
+                <Message />
+            </div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontsize="large" />
+                <form>
+                    <input type="text" name="" id="" placeholder="Type here..."/>
+                    <button className="chat__inputButton"
+                        type="submit">Send Message</button>
+                </form>
+
+                <div className="chat__inputIcons">
+                    <CreditCardIcon fontSize="large"/>
+                    <GifIcon fontSize="large"/>
+                    <EmojiEmotionsIcon fontSize="large"/>
+                </div>
+            </div>
         </div>
     )
 }
