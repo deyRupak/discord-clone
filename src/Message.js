e34f495454ecfd67dd8c570618a568b7313fa9e9
deyRupak <rupakdey9999@gmail.com>
2020-10-15 13:04:08 +0530

    Add Redux States

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Message.css'
+import { Avatar } from '@material-ui/core'
 
 function Message() {
     return (
-        <div>
-            
+        <div className="message">
+            <Avatar />
+            <div className="message__info">
+                <h4>
+                    username
+                    <span className="message__timestamp">
+                        timestamp
+                    </span>
+                </h4>
+
+                <p>message here</p>
+            </div>
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBMCT7ldpD3qNV94rwXo4FzStyOGoI4jsA",
   authDomain: "discord-clone-rdx.firebaseapp.com",
@@ -9,3 +11,12 @@ const firebaseConfig = {
   appId: "1:909253752282:web:239e87468ed0dfe5c73ce3",
   measurementId: "G-M4MSS1CPVP"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
+
