8530becdcc8ce5abe77c4d2f7db4d1166b207de5
deyRupak <rupakdey9999@gmail.com>
2020-10-16 12:15:50 +0530

    Add messageState Redux

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -8,7 +8,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import SendIcon from '@material-ui/icons/Send';
 import HelpIcon from '@material-ui/icons/Help';
 
-function ChatHeader() {
+function ChatHeader({channelName}) {
     return (
         <div className="chatHeader">
             <div className="chatHeader__left">
@@ -16,7 +16,7 @@ function ChatHeader() {
                     <span className="chatHeader__hash">
                         #                    
                     </span>
-                    channel name
+                    {channelName}
                 </h3>
             </div>
 
